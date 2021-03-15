f85151a8a2ebd20d979d23059d3d2d99a9fc5377
Maxim <MVXZ@users.noreply.github.com>
2021-03-15 14:53:59 +0300

    Login

---
+++ Messenger/Controllers/Login/RegisterViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RegisterViewController.swift
+//  Messenger
+//
+//  Created by Панкратьева Ольга on 13.03.2021.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .link
+    }
+    
+
+}
