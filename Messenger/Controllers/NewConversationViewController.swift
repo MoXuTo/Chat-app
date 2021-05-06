76b436852cb05a968ade39eef509d2f7766eac37
Tokimaro <45174024+Tokimaro@users.noreply.github.com>
2021-05-06 11:43:46 +0300

    Fix Photo bug

--- Messenger/Controllers/NewConversationViewController.swift
+++ Messenger/Controllers/NewConversationViewController.swift
@@ -1,22 +1,15 @@
-//
-//  NewConversationViewController.swift
-//  Messenger
-//
-//  Created by Панкратьева Ольга on 13.03.2021.
-//
-
 import UIKit
 import JGProgressHUD
 
-class NewConversationViewController: UIViewController {
-    
-    public var completion: (([String: String]) -> (Void))?
+final class NewConversationViewController: UIViewController {
+
+    public var completion: ((SearchResult) -> (Void))?
 
     private let spinner = JGProgressHUD(style: .dark)
 
     private var users = [[String: String]]()
 
-    private var results = [[String: String]]()
+    private var results = [SearchResult]()
 
     private var hasFetched = false
 
@@ -29,8 +22,8 @@ class NewConversationViewController: UIViewController {
     private let tableView: UITableView = {
         let table = UITableView()
         table.isHidden = true
-        table.register(UITableViewCell.self,
-                       forCellReuseIdentifier: "cell")
+        table.register(NewConversationCell.self,
+                       forCellReuseIdentifier: NewConversationCell.identifier)
         return table
     }()
 
@@ -51,9 +44,9 @@ class NewConversationViewController: UIViewController {
 
         tableView.delegate = self
         tableView.dataSource = self
-        searchBar.delegate = self
 
-        view.backgroundColor = .white
+        searchBar.delegate = self
+        view.backgroundColor = .systemBackground
         navigationController?.navigationBar.topItem?.titleView = searchBar
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Cancel",
                                                             style: .done,
@@ -75,7 +68,6 @@ class NewConversationViewController: UIViewController {
         dismiss(animated: true, completion: nil)
     }
 
-
 }
 
 extension NewConversationViewController: UITableViewDelegate, UITableViewDataSource {
@@ -84,49 +76,52 @@ extension NewConversationViewController: UITableViewDelegate, UITableViewDataSou
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell",
-                                                 for: indexPath)
-        cell.textLabel?.text = results[indexPath.row]["name"]
+        let model = results[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: NewConversationCell.identifier,
+                                                 for: indexPath) as! NewConversationCell
+        cell.configure(with: model)
         return cell
     }
 
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-       tableView.deselectRow(at: indexPath, animated: true)
-       // start conversation
+        tableView.deselectRow(at: indexPath, animated: true)
+        // start conversation
         let targetUserData = results[indexPath.row]
-        
+
         dismiss(animated: true, completion: { [weak self] in
             self?.completion?(targetUserData)
         })
-        
-     }
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 90
+    }
 }
 
 extension NewConversationViewController: UISearchBarDelegate {
 
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-      guard let text = searchBar.text, !text.replacingOccurrences(of: " ", with: "").isEmpty else {
-          return
-      }
+        guard let text = searchBar.text, !text.replacingOccurrences(of: " ", with: "").isEmpty else {
+            return
+        }
 
-      searchBar.resignFirstResponder()
+        searchBar.resignFirstResponder()
 
-      results.removeAll()
-      spinner.show(in: view)
+        results.removeAll()
+        spinner.show(in: view)
 
-      self.searchUsers(query: text)
+        searchUsers(query: text)
     }
 
     func searchUsers(query: String) {
-      // check if array has firebase results
-      if hasFetched {
-          // if it does: filter
-          filterUsers(with: query)
-
-      }
-      else {
-          // if not, fetch then filter
-          DatabaseManager.shared.getAllUsers(completion: { [weak self] result in
+        // check if array has firebase results
+        if hasFetched {
+            // if it does: filter
+            filterUsers(with: query)
+        }
+        else {
+            // if not, fetch then filter
+            DatabaseManager.shared.getAllUsers(completion: { [weak self] result in
                 switch result {
                 case .success(let usersCollection):
                     self?.hasFetched = true
@@ -135,25 +130,38 @@ extension NewConversationViewController: UISearchBarDelegate {
                 case .failure(let error):
                     print("Failed to get usres: \(error)")
                 }
-          })
-
-      }
+            })
+        }
     }
 
     func filterUsers(with term: String) {
-        // update the UI
-        guard hasFetched else {
+        // update the UI: eitehr show results or show no results label
+        guard let currentUserEmail = UserDefaults.standard.value(forKey: "email") as? String, hasFetched else {
             return
-
         }
+
+        let safeEmail = DatabaseManager.safeEmail(emailAddress: currentUserEmail)
 
         self.spinner.dismiss()
 
-        let results: [[String: String]] = self.users.filter({
+        let results: [SearchResult] = users.filter({
+            guard let email = $0["email"], email != safeEmail else {
+                return false
+            }
+
             guard let name = $0["name"]?.lowercased() else {
                 return false
             }
+
             return name.hasPrefix(term.lowercased())
+        }).compactMap({
+
+            guard let email = $0["email"],
+                let name = $0["name"] else {
+                return nil
+            }
+
+            return SearchResult(name: name, email: email)
         })
 
         self.results = results
@@ -163,13 +171,13 @@ extension NewConversationViewController: UISearchBarDelegate {
 
     func updateUI() {
         if results.isEmpty {
-            self.noResultsLabel.isHidden = false
-            self.tableView.isHidden = true
+            noResultsLabel.isHidden = false
+            tableView.isHidden = true
         }
         else {
-            self.noResultsLabel.isHidden = true
-            self.tableView.isHidden = false
-            self.tableView.reloadData()
+            noResultsLabel.isHidden = true
+            tableView.isHidden = false
+            tableView.reloadData()
         }
     }
 
